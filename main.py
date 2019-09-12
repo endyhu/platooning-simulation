fc0ea26312a3575d8ce59a845df47d90bee58897
endyhu <endyhu99@gmail.com>
2019-09-12 23:57:59 +0200

    Update vehicle and road functionality Add new road sprites Update vehicle control parameters

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ WINDOW_HEIGHT = 600
 TITLE = "Platooning Simulator"
 
 background_img = pg.image.load("./assets/grass.png")
-road_img = pg.image.load("./assets/road.png")
+road_top_img = pg.image.load("./assets/road_top.png")
+road_mid_img = pg.image.load("./assets/road_mid.png")
+road_bot_img = pg.image.load("./assets/road_bot.png")
 car_img = pg.image.load("./assets/car.png")
 
 def centerImage(image):
@@ -21,6 +23,7 @@ def centerImage(image):
     image.anchor_y = image.height // 2
 
 def centerVehicleImage(image):
+    # image.anchor_x = image.width // 2
     image.anchor_y = image.height // 2
 
 centerVehicleImage(car_img)
@@ -40,8 +43,39 @@ class Object:
         pass
 
 class RoadObject(Object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, lanes=3):
+        self.lanes = lanes
+        self.lane_width = road_mid_img.height
+
+        self.sprite_batch = pg.graphics.Batch()
+        self.road_sprites = []
+
+        for i in range(8):
+            pos_x = i * 120
+            pos_y = (WINDOW_HEIGHT / 2) - (self.lane_width / 2) + (self.lane_width * (self.lanes - 1) / 2)
+            for j in range(self.lanes):
+                road_img = road_mid_img
+                
+                if j != 0:
+                    pos_y = pos_y - self.lane_width
+                if j == 0:
+                    road_img = road_top_img
+                elif j == self.lanes - 1:
+                    road_img = road_bot_img
+                    
+                self.road_sprites.append(pg.sprite.Sprite(road_img, pos_x, pos_y, batch=self.sprite_batch))
+
+    def draw(self):
+        self.sprite_batch.draw()
+
+    def update(self, velocity, dt):
+        for i, sprite in enumerate(self.road_sprites):
+            sprite.x = sprite.x - velocity * dt
+            
+            if sprite.x < -120:
+                sprite.x = sprite.x + (120 * 8)
+            elif sprite.x > (120 * 7):
+                sprite.x = sprite.x - (120 * 8)
 
 class CarObject(Object):
     def __init__(self, *args, **kwargs):
@@ -52,26 +86,30 @@ class CarObject(Object):
         self.tyre_angle = 0.0
         self.acceleration = 0.0
         self.velocity = 0.0
+        self.velocity_x = 0.0
+        self.velocity_y = 0.0
 
         self.nrm_deceleration = 25.0
         self.brk_deceleration = 100.0
 
-        self.max_tyre_angle = 30.0
+        self.max_tyre_angle = 25.0
         self.max_acceleration = 59.1
         self.max_velocity = 277.8
-        self.max_velocity_r = -69.4
+        self.max_velocity_r = -138.9
 
     def handleKeys(self):
         if keys[key.W]:
             self.acceleration = self.max_acceleration
+            if self.velocity < 0.0:
+                self.acceleration = self.brk_deceleration
         if keys[key.A]:
-            self.steering = -10
+            self.steering = -1
         if keys[key.S]:
             self.acceleration = -self.max_acceleration * 0.8
             if self.velocity > 0.0:
                 self.acceleration = -self.brk_deceleration
         if keys[key.D]:
-            self.steering = 10
+            self.steering = 1
         if keys[key.SPACE]:
             self.acceleration = -np.sign(self.velocity) * self.brk_deceleration
 
@@ -99,11 +137,11 @@ class CarObject(Object):
             if abs(self.tyre_angle) < 5.0:
                 self.tyre_angle = 0.0
             else:
-                self.tyre_angle = self.tyre_angle - (np.sign(self.tyre_angle) * 10 * self.max_tyre_angle) * dt
+                self.tyre_angle = self.tyre_angle - (np.sign(self.tyre_angle) * self.max_tyre_angle * 3) * dt
         else:
-            self.tyre_angle = self.tyre_angle + (self.steering * self.max_tyre_angle) * dt
+            self.tyre_angle = self.tyre_angle + (self.steering * self.max_tyre_angle * 3) * dt
 
-        if self.tyre_angle:
+        if self.tyre_angle != 0.0:
             turning_radius = self.length / np.tan(np.deg2rad(self.tyre_angle))
             angular_velocity = self.velocity / turning_radius
         else:
@@ -123,11 +161,11 @@ class CarObject(Object):
         else:
             self.velocity = self.velocity + self.acceleration * dt
 
-        velocity_x = np.cos(np.deg2rad(self.sprite.rotation))
-        velocity_y = -np.sin(np.deg2rad(self.sprite.rotation))
+        self.velocity_x = self.velocity * np.cos(np.deg2rad(self.sprite.rotation))
+        self.velocity_y = self.velocity * -np.sin(np.deg2rad(self.sprite.rotation))
 
-        self.sprite.x = self.sprite.x + self.velocity * velocity_x * dt
-        self.sprite.y = self.sprite.y + self.velocity * velocity_y * dt
+        # self.sprite.x = self.sprite.x + self.velocity_x * dt
+        self.sprite.y = self.sprite.y + self.velocity_y * dt
 
 class Window(pg.window.Window):
     def __init__(self, *args, **kwargs):
@@ -140,13 +178,8 @@ class Window(pg.window.Window):
 
         self.background = pg.sprite.Sprite(background_img, x=0, y=0)
         
-        self.road = RoadObject(0, (WINDOW_HEIGHT // 2) - (road_img.height // 2), road_img)
-        self.car = CarObject(200, WINDOW_HEIGHT // 2, car_img)
-
-    # def on_key_press(self, symbol, modifiers):
-        # image_data = pg.image.get_buffer_manager().get_color_buffer().get_image_data().data
-        # image_data = np.frombuffer(image_data, dtype=np.uint8).reshape(WINDOW_HEIGHT, WINDOW_WIDTH, 4)
-        # image_data = image_data[:, :, :-1]
+        self.road = RoadObject(5)
+        self.car = CarObject(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, car_img)
 
     def on_key_release(self, symbol, modifiers):
         self.car.on_key_release(symbol, modifiers)
@@ -158,6 +191,7 @@ class Window(pg.window.Window):
         self.car.draw()
 
     def update(self, dt):
+        self.road.update(self.car.velocity_x, dt)
         self.car.handleKeys()
         self.car.update(dt)
 
