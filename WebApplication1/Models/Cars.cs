3d99bad95bd4b0da4f6efb0a4b0afde7cd936cc9
Modmr <mo_42@hotmail.nl>
2020-01-12 14:18:19 +0100

    BACKENDAPI
    For the back end API to be accesable on your local network: Install Conveyor for VS Studio(https://vs-publisher-1448185.gallerycdn.vsassets.io/extensions/vs-publisher-1448185/conveyorbykeyoti/2.2.5/1572296531807/Keyoti.Conveyor.vsix)

---
+++ WebApplication1/Models/Cars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public double leftspeed { get; set; }
+        public double rightspeed { get;set; }
+
+        public bool leftlinesensor { get; set; }
+
+        public bool rightlinesensor { get; set; }
+
+        public double ultrasonicsensor { get; set; }
+    }
+}
