ddacd8b64bf797691a4c53c7b9900431b99f01ab
Modmr <mo_42@hotmail.nl>
2020-01-12 15:18:04 +0100

    update(put) method fixed

--- WebApplication1/Controllers/CarsController.cs
+++ WebApplication1/Controllers/CarsController.cs
@@ -11,26 +11,18 @@ namespace WebApplication1.Controllers
 {
     public class CarsController : ApiController
     {
-        private Car[] Cars = new Car[]
-        {
-        new Car { id = 1, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
-        new Car { id = 2, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
-        new Car { id = 3, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
-        new Car { id = 4, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
-        new Car { id = 5, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0}
-        };
 
         // GET: api/Cars
         [ResponseType(typeof(IEnumerable<Car>))]
         public IEnumerable<Car> Get()
         {
-            return Cars;
+            return Data.Cars;
         }
 
-        // GET: api/Cars/5
+        // GET: api/Data.Cars/5
         public IHttpActionResult Get(int id)
         {
-            var product = Cars.FirstOrDefault((p) => p.id == id);
+            var product = Data.Cars.FirstOrDefault((p) => p.id == id);
             if (product == null)
             {
                 return NotFound();
@@ -41,20 +33,21 @@ namespace WebApplication1.Controllers
         // POST: api/Cars
         public void Post([FromBody]string value)
         {
-            
+
         }
 
-        // PUT: api/Cars/5
-        public IHttpActionResult Put(int id, [FromBody]Car value)
+        // PUT: api/Data.Cars/5
+        [HttpPut]
+        public IHttpActionResult Put([FromBody]Car value)
         {
-            
-            var product = Cars.FirstOrDefault((p) => p.id == value.id);
+
+            var product = Data.Cars.FirstOrDefault((p) => p.id == value.id);
             if (product == null)
             {
                 return NotFound();
             }
-             
-            Cars[product.id - 1] = new Car { id = product.id, leftspeed = product.leftspeed,rightspeed= product.rightspeed, leftlinesensor = product.leftlinesensor, rightlinesensor = product.rightlinesensor, ultrasonicsensor= product.ultrasonicsensor };
+            Car car = value;
+            Data.Cars[car.id - 1] = car; //new Car { id = car.id, leftspeed = car.leftspeed,rightspeed= car.rightspeed, leftlinesensor = car.leftlinesensor, rightlinesensor = car.rightlinesensor, ultrasonicsensor= car.ultrasonicsensor };
             return Ok();
         }
 
@@ -62,5 +55,17 @@ namespace WebApplication1.Controllers
         public void Delete(int id)
         {
         }
+    }
+
+    public class Data
+    {
+        public static Car[] Cars = new Car[]
+        {
+        new Car { id = 1, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
+        new Car { id = 2, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
+        new Car { id = 3, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
+        new Car { id = 4, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0},
+        new Car { id = 5, leftspeed = 0, rightspeed= 0, leftlinesensor= false, rightlinesensor=false,ultrasonicsensor=0}
+        };
     }
 }
