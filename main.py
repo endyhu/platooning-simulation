10f34bbfe3f1599794bc2ae8169a01147ee43d9e
endyhu <endyhu99@gmail.com>
2020-01-14 11:01:27 +0100

    .

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-import requests
-import json
-import time
 import ai
+import time
+import json
+import requests
 
 url = "http://192.168.43.92:3000/api/cars/"
 
-def getData(id = ""):
+def getData(id=""):
     global url
     request_data = requests.get(url+str(id), verify=False)
     return request_data.json()
 
 def updateData(id, leftspeed, rightspeed, leftlinesensor, rightlinesensor, ultrasonicsensor):
     global url
-    payload = {"id":id,"leftspeed":leftspeed,"rightspeed":rightspeed,"leftlinesensor":leftlinesensor,"rightlinesensor":rightlinesensor,"ultrasonicsensor":ultrasonicsensor}
+    payload = {"id": id, "leftspeed": leftspeed, "rightspeed": rightspeed, "leftlinesensor": leftlinesensor,
+               "rightlinesensor": rightlinesensor, "ultrasonicsensor": ultrasonicsensor}
     headers = {'content-type': 'application/json'}
-    r = requests.put(url, data=json.dumps(payload), headers=headers, verify=False)
+    r = requests.put(url, data=json.dumps(payload),
+                     headers=headers, verify=False)
     return r.status_code
 
 def main():
@@ -24,9 +26,10 @@ def main():
     while True:
         output_data = [i for i in getData(1).values()]
         output_data.pop(0)
-        # prediction = ai.predict()
-        print(estimator.preprocess(output_data))
+
+        prediction = estimator.predict(output_data)
+        print(prediction)
 
         time.sleep(1)
-        
+
 main()
