2ea0bf68205a99ed7a0cd0bf610d8f8a048ea7df
Modmr <mo_42@hotmail.nl>
2020-01-14 10:05:44 +0100

    temp

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import ai
 
 url = "http://192.168.43.92:3000/api/cars/"
 
-def getData(id=""):
+def getData(id = ""):
     global url
     request_data = requests.get(url+str(id), verify=False)
     return request_data.json()
 
-def updateData(id,leftspeed,rightspeed,leftlinesensor,rightlinesensor,ultrasonicsensor):
+def updateData(id, leftspeed, rightspeed, leftlinesensor, rightlinesensor, ultrasonicsensor):
     global url
     payload = {"id":id,"leftspeed":leftspeed,"rightspeed":rightspeed,"leftlinesensor":leftlinesensor,"rightlinesensor":rightlinesensor,"ultrasonicsensor":ultrasonicsensor}
     headers = {'content-type': 'application/json'}
@@ -22,9 +22,10 @@ def main():
     estimator.load("_705.76_00057137_06_1452")
 
     while True:
-        output_data = getData(1)
+        output_data = [i for i in getData(1).values()]
+        output_data.pop(0)
         # prediction = ai.predict()
-        print(output_data.values())
+        print(estimator.preprocess(output_data))
 
         time.sleep(1)
         
